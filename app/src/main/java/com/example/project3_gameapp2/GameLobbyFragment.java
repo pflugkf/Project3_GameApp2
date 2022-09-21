a3e39485f11ba5ad3dacbee68f69449e79468ec6
Adrianna McKeown <amckeow3@uncc.edu>
2022-09-21 17:49:42 -0400

    leave game button hidden in game lobby

--- app/src/main/java/com/example/project3_gameapp2/GameLobbyFragment.java
+++ app/src/main/java/com/example/project3_gameapp2/GameLobbyFragment.java
@@ -40,54 +40,18 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link GameLobbyFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class GameLobbyFragment extends Fragment {
     private static final String TAG = "game lobby fragment";
     private FirebaseAuth mAuth;
     FirebaseFirestore db;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public GameLobbyFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GameLobbyFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GameLobbyFragment newInstance(String param1, String param2) {
-        GameLobbyFragment fragment = new GameLobbyFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -107,6 +71,7 @@ public class GameLobbyFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle(R.string.game_lobby_fragment);
+        getActivity().findViewById(R.id.toolbar).findViewById(R.id.buttonLeaveGame).setVisibility(View.INVISIBLE);
 
         db = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
